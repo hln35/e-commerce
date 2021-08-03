012866251e69ee5ef08c9cba0e29bc28fb528b40
hln35 <hln35@cam.ac.uk>
2021-08-03 14:03:59 +0100

    content

--- src/components/Home/Intro/intro_gypsum.jsx
+++ src/components/Home/Intro/intro_gypsum.jsx
@@ -32,6 +32,9 @@ Tuy nhiên vẫn có rất nhiều người chưa thực sự hiểu về trần
                 <Typography style={{fontWeight:"bold"}}>
                 Đồng thời Tân Hải Vân cũng sẽ giới thiệu về các gói thi công trần vách thạch cao hoàn thiện của công ty chúng tôi.
                 </Typography>
+                <Typography style={{color:'#ed9f51'}}>
+                    Với tư cách là một trong những trung tâm phân phối lớn nhất của Vĩnh Tường tại miền bắc và có gần 20 năm hoạt động trong lĩnh vực trần, chúng tôi có đội ngũ thợ thi công kinh nghiệm cùng nguồn hàng cạnh tranh với khả năng hoàn thành những công trình với quy chuẩn khắt khe nhất trên một mức chi phí phù hợp.
+                </Typography>
                 <br/>
                 <Typography variant="h6" style={{color:'#b5934c'}}>
                     Định nghĩa về trần thạch cao
@@ -48,7 +51,7 @@ Tuy nhiên vẫn có rất nhiều người chưa thực sự hiểu về trần
                             <Home/>
                         </ListItemAvatar>
                         <ListItemText>
-                    Khung xương: Cố định hệ trần theo một khung xương có sẵn. Tối ưu tính vững chắc để lên thạch cao & sơn bả.
+                    Khung xương: Giúp cố định hệ trần. Nó là kết cấu chính đem đến tính vững chắc cho hệ trần thạch cao.
                         </ListItemText>
                     </ListItem>
                     <ListItem>
@@ -56,7 +59,7 @@ Tuy nhiên vẫn có rất nhiều người chưa thực sự hiểu về trần
                             <Home/>
                         </ListItemAvatar>
                         <ListItemText>
-                        Tấm trần thạch cao: là bộ phận kết nối trực tiếp với khung xương thông qua vít chuyên dụng. Tạo nên độ phẳng cho trần.                      
+                        Tấm trần thạch cao: là bộ phận kết nối trực tiếp với khung xương thông qua vít chuyên dụng. Đây sẽ là lớp trần mà chúng ta sẽ thấy khi công trình được hoàn thiện.                     
                       </ListItemText>
                     </ListItem>
                     <ListItem>
@@ -64,18 +67,18 @@ Tuy nhiên vẫn có rất nhiều người chưa thực sự hiểu về trần
                             <Home/>
                         </ListItemAvatar>
                         <ListItemText>
-                        Sơn bả (Áp dụng với trần chìm): tạo độ mịn & đều màu cho mặt trần.
+                        Sơn bả (Áp dụng với trần chìm): tạo độ mịn & đều màu cho mặt trần, là bước hoàn thiện sau khi phần thô đã được hoàn thành
                         </ListItemText>
                     </ListItem>
                 </List>
                 <Typography gutterBottom>
-                Có thể các bạn đã tiếp xúc với tấm trần nhựa hay trần nhựa giả gỗ, song trần nhà làm từ thạch cao được ưa chuộng hơn rất nhiều bởi có vô vàn ưu điểm siêu việt được các gia đình ưa thích. Cụ thể như sau:
+                Tấm trần nhựa hay trần nhựa giả gỗ xuất hiện từ lâu đã trở nên quen thuộc với chúng ta, song giờ đây không còn được ưa dùng trần nhà làm từ thạch cao được ưa chuộng hơn rất nhiều bởi có vô vàn ưu điểm siêu việt được các gia đình ưa thích. Cụ thể như sau:
                 </Typography>
                 <Typography>
                 - Thạch cao có trọng lượng nhẹ hơn khoảng 10 lần so với tường gạch truyền thống.
                 </Typography>
                 <Typography>
-                - Thạch cao có tính năng linh động, dễ tháo lắp. Xây dựng nhanh gọn mà không ảnh hưởng tới cấy trúc trần ngôi nhà cũng như hệ thống dầm chung của cả tòa nhà. Nếu hỏng bạn có thể lắp mới nhanh chóng
+                - Thạch cao có khả năng linh động cao, vô cùng dễ dàng dễ tháo lắp. Vậy nên hệ thống trần vách thạch cao được lắp đặt nhanh gọn mà không ảnh hưởng tới cấy trúc trần ngôi nhà cũng như hệ thống dầm chung của cả tòa nhà. 
                 </Typography>
                 <Typography>
                 - Trần có độ an toàn cao nhờ khung xương trần vững chắc. Không chứa các chất độc hại trong quá trình sử dụng hoặc phát ra khí độc.
@@ -84,7 +87,7 @@ Tuy nhiên vẫn có rất nhiều người chưa thực sự hiểu về trần
                 - Về khả năng chống cháy, thạch cao được xem là một nguyên liệu tuyệt vời nhờ vào lõi đã được điều chỉnh khiến nó có tác dụng làm chậm lại quá trình truyền nhiệt và sự lan truyền của lửa, có thể lên tới 150 phút, một thứ tối quan trọng khi không may xảy ra hỏa hoạn. Ngoài ra, tính năng cách nhiệt của tấm thạch cao còn đóng vai trò chống nóng và giảm lượng điện năng tiêu thụ của điều hòa vào mùa hè.   
                 </Typography>
                 <Typography>
-                   - Thạch cao cũng có tính tiêu âm, giảm thiểu sự truyền thanh cho âm thanh nên cũng được áp dụng rộng rãi trong các không gian sinh hoạt cần sự riêng tư như chung cư, văn phòng,...
+                   - Thạch cao cũng có tính tiêu âm, với tác dụng cản trở sự lan truyền của âm thanh nên cũng được áp dụng rộng rãi trong các không gian sinh hoạt cần sự riêng tư như chung cư, văn phòng,...
                 </Typography>
                 <Typography>
                 - Tuổi thọ của trần nhà thạch cao cũng tương đồng với độ bền của đồ nội thất nên giúp tiết kiệm được tối đa chi phí bảo dưỡng cũng như đảm bảo an toàn cho những thành viên trong gia đình. 
@@ -101,7 +104,7 @@ Tuy nhiên vẫn có rất nhiều người chưa thực sự hiểu về trần
                     Hiện nay phổ biến 2 loại trần là trần chìm và trần thả.
                 </Typography>
                 <Typography>
-                Trần chìm là một loại trần thạch cao có cấu tạo khung xương được ẩn giấu toàn bộ bên trên các tấm thạch cao, bạn sẽ không thể nhìn thấy các khung xương này, nhìn giống như trần bê tông bình thường. Trần chìm được thiết kế bao gồm khung xương và các tấm thạch cao, trong đó khung xương có tác dụng để treo các tấm thạch cao. Khung định hình bằng nhôm kẽm chữ U được bắt vít gắn kết với nhau, sau đó người ta ghép từng tấm thạch cao vào với nhau.
+                Trần chìm là hệ thống trần thạch cao có cấu tạo khung xương được ẩn giấu toàn bộ bên trên các tấm thạch cao, bạn sẽ không thể nhìn thấy dấu vết nào cả khi trần đã hoàn thiện, nói cách khác nó sẽ giống như một lớp trần bê tông bình thường. Trần chìm được thiết kế bao gồm khung xương và các tấm thạch cao, khung được định hình bằng các thanh thép hình chữ U được bắt vít gắn kết với nhau, sau đó người ta sẽ ghép từng tấm thạch cao vào với nhau (như hình dưới đây).
                 </Typography>
             </CardContent>
             <CardMedia image={TranChim} className={classes.media} title={"trần thạch cao chìm"} alt="cấu tạo trần chìm"/>
@@ -110,13 +113,13 @@ Tuy nhiên vẫn có rất nhiều người chưa thực sự hiểu về trần
                     Ưu điểm:
                 </Typography>
                 <Typography>
-                    - Trần thạch cao chìm có ưu điểm về tính thẩm mỹ cao khi dễ dàng trang trí hoa văn, họa tiết theo sở thích.
+                    - Trần thạch cao chìm có tính thẩm mỹ và linh hoạt cao khi chúng ta có thể dễ dàng trang trí hoa văn, họa tiết theo sở thích.
                 </Typography>
                 <Typography>
-                    - Có thể cắt ghép kết hợp với nhiều loại đèn trang trí khác nhau mang đến vẻ đẹp sang trọng, tinh tế cho mỗi căn phòng.
+                    - Cũng nhờ tính linh hoạt trần chìm có thể kết hợp với nhiều loại đèn trang trí khác nhau mang đến vẻ đẹp sang trọng, tinh tế cho mỗi căn phòng.
                 </Typography>
                 <Typography>
-                    - Trần chìm dễ dàng ứng dụng trong nhiều thiết kế nội thất biệt thự, nhà phố, văn phòng…. khác nhau dù diện tích lớn hay nhỏ thì mẫu trần này cũng rất thích hợp.
+                    - Trần chìm dễ dàng ứng dụng trong nhiều thiết kế nội thất đa dạng cho dù diện tích lớn hay nhỏ thì mẫu trần này cũng rất thích hợp.
                 </Typography>
                 <Typography>
                     - Trọng lượng nhẹ, an toàn cho người sử dụng.
@@ -131,13 +134,13 @@ Tuy nhiên vẫn có rất nhiều người chưa thực sự hiểu về trần
         <Card>
             <CardContent>
                 <Typography>
-                    Về trần nổi, hay còn gọi là trần thạch cao thả, là loại trần có thiết kế với tấm trang trí và một phần khung xương bị lộ ra ngoài. Nó có thể che đi các khuyết điểm của công trình như: đường dây điện, ống nước…đặt ở dưới trần bê tông hoặc dưới mái tôn, mái ngói. Trần thạch cao nổi thường được sử dụng trong việc thi công trần thạch cao phòng bếp, phòng tắm,... do chúng có thể thay thế vô cùng dễ dàng.
+                    Về trần nổi, hay còn gọi là trần thạch cao thả, là loại trần có thiết kế với tấm trang trí và một phần khung xương bị lộ ra ngoài. Nó có thể che đi các khuyết điểm của công trình ở phía trên như: đường dây điện, ống nước…đặt ở dưới trần bê tông hoặc dưới mái tôn, mái ngói. Trần thạch cao nổi thường được sử dụng trong việc thi công trần thạch cao phòng bếp, phòng tắm,... do chúng có thể thay thế vô cùng dễ dàng.
                 </Typography>
                 <Typography style={{fontWeight:'bold'}}>
                     Ngoài ra chúng còn có một vài ưu điểm nữa các bạn có thể tham khảo:
                 </Typography>
                 <Typography>
-                    - Trần thả được lắp đặt vô cùng nhanh gọn, chúng có thể được lắp ngay trong khi công trình trong quá trình sử dụng
+                    - Trần thả được lắp đặt vô cùng nhanh gọn, chúng có thể được lắp ngay trong khi công trình trong quá trình sử dụng.
                 </Typography>
                 <Typography>
                     - Trần thạch cao thả có thể giúp bạn tiết kiệm tối đa chi phí lắp đặt cũng như sửa chữa.
